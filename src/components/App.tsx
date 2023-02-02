b6906effc1efa27b3e8da7d2005d31880c201432
Nikolay Brovkin <bronik004@gmail.com>
2023-02-02 11:13:27 +0300

    feat: added routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Counter from './counter';
 import './index.scss'
+import {Link, Route, Routes} from "react-router-dom";
+import AboutPage from "../Pages/AboutPage/AboutPage";
+import MainPage from "../Pages/MainPage/MainPage";
 
 const App = () => {
     return (
         <div className='app'>
-            some text
-            <Counter/>
+            <Link to='/'>Главная</Link>
+            <Link to='/about'>О нас</Link>
+            <Routes>
+                <Route path={'/about'} element={<AboutPage />}/>
+                <Route path={'/'} element={<MainPage />}/>
+            </Routes>
         </div>
     );
 };
