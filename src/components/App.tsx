2dbd02d0e7e020f40fe036173d16f925d3fb9a9c
Nikolay Brovkin <bronik004@gmail.com>
2023-02-03 12:05:17 +0300

    feat: added theme and ThemeContext

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,20 @@
-import React, {Suspense} from 'react';
+import React, {Suspense, useContext, useState} from 'react';
 import '../styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageAsync} from "../Pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "../Pages/MainPage/MainPage.async";
+import {Theme, ThemeContext} from "../theme/ThemeContext";
 
 const App = () => {
+    const {theme, setTheme} = useContext(ThemeContext);
+    function toggleTheme() {
+        setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
+    }
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
             <Link to='/'>Главная</Link>
             <Link to='/about'>О нас</Link>
+            <button onClick={toggleTheme}>Toggle Theme</button>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
                     <Route path={'/about'} element={<AboutPageAsync/>}/>
