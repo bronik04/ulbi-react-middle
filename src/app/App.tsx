713ba8567f94705c23fe48da11eb0a6890ea2b7d
Nikolay Brovkin <bronik004@gmail.com>
2023-02-06 13:14:42 +0300

    refactor: updated pages structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import React, {Suspense} from 'react';
 import './styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
-import {AboutPageAsync} from "Pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "Pages/MainPage/MainPage.async";
 import {useTheme} from "app/providers/ThemePrivider";
 import {classNames} from "helpers/classNames/classNames";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
@@ -15,8 +15,8 @@ const App = () => {
             <button onClick={toggleTheme}>Toggle Theme</button>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
         </div>
