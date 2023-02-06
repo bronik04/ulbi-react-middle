ddaaa8f214b86f3338dbc8d451af5c1daeec5b39
Nikolay Brovkin <bronik004@gmail.com>
2023-02-06 22:47:24 +0300

    feat: added routeConfig and AppRoute

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import './styles/index.scss'
-import {Link, Route, Routes} from "react-router-dom";
+import {Link} from "react-router-dom";
 import {useTheme} from "app/providers/ThemePrivider";
 import {classNames} from "shared/lib/classNames/classNames";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
@@ -13,12 +12,7 @@ const App = () => {
             <Link to='/'>Главная</Link>
             <Link to='/about'>О нас</Link>
             <button onClick={toggleTheme}>Toggle Theme</button>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                    <Route path={'/'} element={<MainPage/>}/>
-                </Routes>
-            </Suspense>
+            <AppRouter/>
         </div>
     );
 };
