b427b5c367d5b7559f4fd3aaa2e988f8ca5d04b4
Nikolay Brovkin <bronik004@gmail.com>
2023-02-11 16:39:10 +0300

    test: translation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './styles/index.scss'
 import {useTheme} from "app/providers/ThemePrivider";
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
 import {Sidebar} from "widgets/Sidebar";
+import {useTranslation} from "react-i18next";
+
+const component = () => {
+    useTranslation()
+}
 
 const App = () => {
     const {theme} = useTheme();
     return (
-        <div className={classNames('app',{}, [theme])}>
-            <Navbar/>
-            <main className='content-page'>
-                <Sidebar/>
-                <AppRouter/>
-            </main>
+        <div className={classNames('app', {}, [theme])}>
+            <Suspense fallback="">
+                <Navbar/>
+                <main className='content-page'>
+                    <Sidebar/>
+                    <AppRouter/>
+                </main>
+            </Suspense>
         </div>
     );
 };
