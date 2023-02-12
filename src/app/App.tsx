530ae50b57cabfbb1cb590ef41a23843f042394a
Nikolay Brovkin <bronik004@gmail.com>
2023-02-12 23:34:26 +0300

    test: added test translation component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,8 +7,17 @@ import {Navbar} from "widgets/Navbar";
 import {Sidebar} from "widgets/Sidebar";
 import {useTranslation} from "react-i18next";
 
-const component = () => {
-    useTranslation()
+const Component = () => {
+   const {t, i18n} = useTranslation();
+
+   const toggle = () => {
+       i18n.changeLanguage(i18n.language === 'en'? 'ru' : 'en');
+   }
+
+   return <div>
+       <button onClick={toggle}> {t('Перевод')}</button>
+       {t('Тестовый пример')}
+   </div>
 }
 
 const App = () => {
@@ -17,6 +26,7 @@ const App = () => {
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
                 <Navbar/>
+                <Component/>
                 <main className='content-page'>
                     <Sidebar/>
                     <AppRouter/>
