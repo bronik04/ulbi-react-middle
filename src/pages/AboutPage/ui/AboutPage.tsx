76104242d21a914c7e30e8a7d73cd282540e3877
Nikolay Brovkin <bronik004@gmail.com>
2023-02-14 10:20:55 +0300

    fix: fixed bug and added main page translation

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {useTranslation} from "react-i18next";
 
 const AboutPage = () => {
+    const {t} = useTranslation();
     return (
         <div>
-            <h1>About page</h1>
+            <h1>{t('О сайте')}</h1>
         </div>
     );
 };
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {useTranslation} from "react-i18next";
 
 const MainPage = () => {
+    const { t } = useTranslation();
     return (
         <div>
-            <h1>Main page</h1>
+            <h1>{t('Главная страница')}</h1>
         </div>
     );
 };
