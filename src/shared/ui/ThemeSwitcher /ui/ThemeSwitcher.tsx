4290809cfcab259d578971d04c9543e6d27df337
Nikolay Brovkin <bronik004@gmail.com>
2023-02-16 11:07:26 +0300

    feat: added i18next eslint plugin

--- src/shared/ui/ThemeSwitcher /ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher /ui/ThemeSwitcher.tsx
@@ -3,7 +3,6 @@ import { Theme, useTheme } from 'app/providers/ThemePrivider';
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
-import cls from './ThemeSwitcher.module.scss';
 
 interface ThemeSwitcherProps {
     className?: string
@@ -14,7 +13,7 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
 
   return (
     <Button
-      className={classNames(cls.themeSwitcher, {}, [className])}
+      className={classNames('', {}, [className])}
       onClick={toggleTheme}
       theme={ThemeButton.CLEAR}
     >
