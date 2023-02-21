d510e2e26a3bded89d803921694548573d42329e
Nikolay Brovkin <bronik004@gmail.com>
2023-02-21 23:03:27 +0300

    fix: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,6 +36,7 @@ module.exports = {
     'import/no-extraneous-dependencies': 'warn',
     'no-underscore-dangle': 'off',
     'i18next/no-literal-string': ['error', { markupOnly: true }],
+    'max-len': ['error', { ignoreComments: true }],
   },
   globals: {
     __IS_DEV__: true,
