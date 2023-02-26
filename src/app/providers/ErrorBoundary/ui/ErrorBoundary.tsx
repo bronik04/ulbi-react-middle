7b08a9a4b790281dee4c7eddb0f15f27e062a4d8
Nikolay Brovkin <bronik004@gmail.com>
2023-02-26 23:35:50 +0300

    feat: added ErrorBoundary

---
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -0,0 +1,42 @@
+import React, { ErrorInfo, ReactNode } from 'react';
+import { withTranslation } from 'react-i18next';
+
+interface ErrorBoundaryProps {
+    children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+    hasError: boolean;
+}
+
+class ErrorBoundary
+    extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+    constructor(props: ErrorBoundaryProps) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error: Error) {
+        // Обновить состояние с тем, чтобы следующий рендер показал запасной UI.
+        return { hasError: true };
+    }
+
+    componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+        // Можно также сохранить информацию об ошибке в соответствующую службу журнала ошибок
+        console.log(error, errorInfo);
+    }
+
+    render() {
+        const { hasError } = this.state;
+        const { children } = this.props;
+        if (hasError) {
+            // Можно отрендерить запасной UI произвольного вида
+            return <h1>Что-то пошло не так.</h1>;
+        }
+
+        return children;
+    }
+}
+
+// export default withTranslation()(ErrorBoundary);
+export default ErrorBoundary;
