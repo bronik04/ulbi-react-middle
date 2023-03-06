ba4c85ab5f11f47fb50da6699a67227ce2d1c8e3
Nikolay Brovkin <bronik004@gmail.com>
2023-03-06 22:40:12 +0300

    feat: update jest config

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -38,6 +38,7 @@ export default {
         '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
     ],
     rootDir: '../../',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
 
