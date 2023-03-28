98285f30b065d0f90f3f47820ee7ee70be882051
Nikolay Brovkin <bronik004@gmail.com>
2023-03-28 13:29:40 +0300

    feat: added modal styles and animation

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,18 +1,36 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import cls from './Modal.module.scss';
 
 interface ModalProps {
     className?: string,
     children?: ReactNode,
+    isOpen?: boolean,
+    onClose?: () => void
 }
 
-export const Modal = ({ className, children }: ModalProps) => (
-    <div className={classNames(cls.modal, {}, [className])}>
-        <div className={cls.overlay}>
-            <div className={cls.content}>
-                {children}
+export const Modal = (props: ModalProps) => {
+    const {
+        className, children, isOpen, onClose,
+    } = props;
+
+    const mods = {
+        [cls.opened]: isOpen,
+    };
+
+    const handleClose = () => {
+        if (onClose) onClose();
+    };
+
+    const handleContentClick = (e: React.MouseEvent) => e.stopPropagation();
+
+    return (
+        <div className={classNames(cls.modal, mods, [className])}>
+            <div className={cls.overlay} onClick={handleClose}>
+                <div className={cls.content} onClick={handleContentClick}>
+                    {children}
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
