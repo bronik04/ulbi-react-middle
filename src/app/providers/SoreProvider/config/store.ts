ae996732aabeb173318c4ad9dd596c8c94fbdace
Nikolay Brovkin <bronik004@gmail.com>
2023-04-01 19:43:32 +0300

    feat: update store

--- src/app/providers/SoreProvider/config/store.ts
+++ src/app/providers/SoreProvider/config/store.ts
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { StateSchema } from './StateSchema';
 
-export function createReduxStore() {
-    return configureStore({
+export function createReduxStore(initialState?: StateSchema) {
+    return configureStore<StateSchema>({
         reducer: {},
         devTools: __IS_DEV__,
     });
