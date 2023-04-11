6dc7721b979f0b927779cdfd80157722049ed85a
Nikolay Brovkin <bronik004@gmail.com>
2023-04-11 09:39:00 +0300

    feat: added localstorage

--- src/entities/User/model/services/loginByUsername/loginByUsername.ts
+++ src/entities/User/model/services/loginByUsername/loginByUsername.ts
@@ -2,6 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { User, userActions } from 'entities/User';
 import i18n from 'shared/config/i18n/i18n';
+import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
 interface loginByUsernameProps {
     username: string;
@@ -16,7 +17,10 @@ export const loginByUsername = createAsyncThunk<User, loginByUsernameProps, { re
             if (!response.data) {
                 throw new Error();
             }
-
+            localStorage.setItem(
+                USER_LOCALSTORAGE_KEY,
+                JSON.stringify(response.data),
+            );
             thunkAPI.dispatch(userActions.setAuthData(response.data));
 
             return response.data;
