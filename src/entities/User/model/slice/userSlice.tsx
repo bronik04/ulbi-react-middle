285b98d52171d3994afb8d8f3927720368583bde
Nikolay Brovkin <bronik004@gmail.com>
2023-04-11 13:36:36 +0300

    feat: added auth to navbar

--- src/entities/User/model/slice/userSlice.tsx
+++ src/entities/User/model/slice/userSlice.tsx
@@ -1,5 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { User, UserSchema } from 'entities/User/model/types/user';
+import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 
 const initialState: UserSchema = {};
 export const userSlice = createSlice({
@@ -8,6 +9,16 @@ export const userSlice = createSlice({
     reducers: {
         setAuthData: (state, action: PayloadAction<User>) => {
             state.authData = action.payload;
+        },
+        initAuthData: (state) => {
+            const user = localStorage.getItem(USER_LOCALSTORAGE_KEY);
+            if (user) {
+                state.authData = JSON.parse(user);
+            }
+        },
+        logout: (state) => {
+            state.authData = undefined;
+            localStorage.removeItem(USER_LOCALSTORAGE_KEY);
         },
     },
 });
