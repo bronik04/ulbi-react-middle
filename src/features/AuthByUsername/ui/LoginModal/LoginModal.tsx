360095b672de8563d07792c04f175c05efab8183
Nikolay Brovkin <bronik004@gmail.com>
2023-04-14 14:42:00 +0300

    feat: added LoginFormAsync to LoginModal

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,6 +1,10 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Modal } from 'shared/ui/Modal/Modal';
-import { LoginForm } from '../LoginForm/LoginForm';
+import { Suspense } from 'react';
+import { Loader } from 'shared/ui/Loader/Loader';
+import {
+    LoginFormAsync,
+} from '../LoginForm/LoginForm.async';
 
 interface LoginModalProps {
     className?: string;
@@ -15,6 +19,8 @@ export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => (
         onClose={onClose}
         lazy
     >
-        <LoginForm />
+        <Suspense fallback={<Loader />}>
+            <LoginFormAsync />
+        </Suspense>
     </Modal>
 );
