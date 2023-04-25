30f2c5dd4d556d3510ce0af5360ec2dd0bbcebfe
Nikolay Brovkin <bronik004@gmail.com>
2023-04-25 21:53:59 +0300

    feat: added reducerManager to store

--- src/app/providers/SoreProvider/config/store.ts
+++ src/app/providers/SoreProvider/config/store.ts
@@ -1,16 +1,21 @@
 import { configureStore, ReducersMapObject } from '@reduxjs/toolkit';
 import { userReducer } from 'entities/User';
 import { loginReducer } from 'features/AuthByUsername';
+import {
+    createReducerManager,
+} from 'app/providers/SoreProvider/config/reducerManager';
 import { StateSchema } from './StateSchema';
 
 export function createReduxStore(initialState?: StateSchema) {
-    const rootReducer: ReducersMapObject<StateSchema> = {
+    const rootReducers: ReducersMapObject<StateSchema> = {
         user: userReducer,
         loginForm: loginReducer,
     };
 
+    const reducerManager = createReducerManager(rootReducers);
+
     const store = configureStore<StateSchema>({
-        reducer: rootReducer,
+        reducer: rootReducers,
         devTools: __IS_DEV__,
     });
 
